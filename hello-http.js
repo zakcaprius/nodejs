4898f82fdf97f962d1924b73c7e9b0d0dd98b8ef
zakcaprius <zakcaprius@gmail.com>
2017-12-18 12:17:46 +0400

    hola

--- hello-http.js
+++ hello-http.js
@@ -2,7 +2,7 @@ var http = require('http');
 http.createServer(function (req ,res) {
     res.writeHead(200, {'Content-Type': 'test/html'});
     
-    res.end('HELLO Host/Pod: ' + process.env.HOSTNAME + '\n');
+    res.end('Hola Host/Pod: ' + process.env.HOSTNAME + '\n');
 
 }).listen(8000, '0.0.0.0');
 console.log('Server running at http://127.0.0.1:8000/');
